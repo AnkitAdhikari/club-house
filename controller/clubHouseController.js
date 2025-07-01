008b2f10a82ccc5b4139e4b410ac637998f8a661
AnkitAdhikari <ankitadhikari31512@gmail.com>
2025-07-01 23:56:12 +0545

    fix login issue by modifing express validator custom validation to return true if all cases is passed for a certain field and also migrated to pnpm

--- controller/clubHouseController.js
+++ controller/clubHouseController.js
@@ -1,147 +1,202 @@
 const passport = require("passport");
-const { getMessages, deleteMessageById, updateMembershipStatus, updateAdminStatus, insertNewMsg, insertUser, checkUserExistance } = require("../database/query");
-const { body, validationResult } = require('express-validator');
+const {
+  getMessages,
+  deleteMessageById,
+  updateMembershipStatus,
+  updateAdminStatus,
+  insertNewMsg,
+  insertUser,
+  checkUserExistance,
+} = require("../database/query");
+const { body, validationResult } = require("express-validator");
 
 const messageValidation = [
-    body('title').trim().notEmpty().withMessage('title cannot be empty'),
-    body('message').trim().notEmpty().withMessage('Message cannot be empty')
-]
+  body("title").trim().notEmpty().withMessage("title cannot be empty"),
+  body("message").trim().notEmpty().withMessage("Message cannot be empty"),
+];
 
 const signupValidation = [
-    body('firstName').trim().notEmpty().withMessage("first name cannot be empty").isAlpha().withMessage("name can only be alphabets"),
-    body('lastName').trim().notEmpty().withMessage('last name cannot be empty').isAlpha().withMessage('name can only be alphabets'),
-    body('username').custom(async (value) => {
-        let existingUser = await checkUserExistance(value);
-        if (!existingUser) {
-            throw new Error("username already in use");
-        }
-    }),
-    body('password').custom((value) => {
-        if (value[0].trim() !== value[1].trim()) throw new Error("both password must be same");
-        if (value[0].trim() == '') throw new Error('password cannot be empty');
-        if (value[0].length < 0) throw new Error('password too short');
-    })
-]
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("first name cannot be empty")
+    .isAlpha()
+    .withMessage("name can only be alphabets"),
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .withMessage("last name cannot be empty")
+    .isAlpha()
+    .withMessage("name can only be alphabets"),
+  body("username").custom(async (value) => {
+    const existingUser = await checkUserExistance(value);
+    if (existingUser) {
+      throw new Error("username already in use");
+    }
+    return true;
+  }),
+  body("password").custom((value) => {
+    console.log(value, "herererer in validation");
+    if (!value[0] || !value[1]) {
+      throw new Error("Both password fields are required");
+    }
+    if (typeof value[0] !== "string" || typeof value[1] !== "string") {
+      throw new Error("Password fields must be strings");
+    }
+    if (value[0].trim() === "") {
+      throw new Error("Password cannot be empty");
+    }
+    if (value[0].trim() !== value[1].trim()) {
+      throw new Error("Both passwords must match");
+    }
+    if (value[0].length < 6) {
+      throw new Error("Password must be at least 6 characters long");
+    }
+    return true;
+  }),
+];
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 async function getHomePage(req, res) {
-
-    const messages = await getMessages();
-    res.render('home', { messages, pageTitle: "Home", });
+  const messages = await getMessages();
+  res.render("home", { messages, pageTitle: "Home" });
 }
 
 async function getSignUp(req, res) {
-    res.render('signup', { pageTitle: 'Sign Up' });
+  res.render("signup", { pageTitle: "Sign Up" });
 }
 
 const postSignUp = [
-    signupValidation,
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).render('signup', { pageTitle: "SignUp", errors: errors.array(), });
-        }
-        const { firstName, lastName, username, password: [password] } = req.body;
-        const salt = bcrypt.genSaltSync(10);
-        const hashedPass = bcrypt.hashSync(password, salt);
-        try {
-            await insertUser(firstName, lastName, username, hashedPass);
-            res.redirect('/');
-        } catch (error) {
-            throw new Error(error);
-        }
+  signupValidation,
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(req.body);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("signup", { pageTitle: "SignUp", errors: errors.array() });
     }
-]
+    const {
+      firstName,
+      lastName,
+      username,
+      password: [password],
+    } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPass = bcrypt.hashSync(password, salt);
+    try {
+      await insertUser(firstName, lastName, username, hashedPass);
+      res.redirect("/");
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+];
 
 async function getLogIn(req, res) {
-    res.render('login', { pageTitle: "Log In" });
+  res.render("login", { pageTitle: "Log In" });
 }
 
-const postLogIn = passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login'
+const postLogIn = passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
 });
 
 async function logOutUser(req, res) {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        res.redirect('/');
-    });
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 }
 
 async function getMembership(req, res) {
-    res.render('membership', { pageTitle: "Membership" });
+  res.render("membership", { pageTitle: "Membership" });
 }
 
 async function getNewMsg(req, res) {
-    res.render('newmsg', { pageTitle: 'Create Message' });
+  res.render("newmsg", { pageTitle: "Create Message" });
 }
 
 async function postMembership(req, res) {
-    try {
-
-        const { memcode } = req.body
-        const userId = req.user.id;
-        if (memcode === "member123") {
-            await updateMembershipStatus(userId);
-            res.redirect('/');
-        } else {
-            throw new Error("Invalid membership code");
-        }
-    } catch (err) {
-        throw new Error(err);
+  try {
+    const { memcode } = req.body;
+    const userId = req.user.id;
+    if (memcode === "member123") {
+      await updateMembershipStatus(userId);
+      res.redirect("/");
+    } else {
+      throw new Error("Invalid membership code");
     }
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 async function getAdmin(req, res) {
-    res.render('admin', { pageTitle: "Admin" });
+  res.render("admin", { pageTitle: "Admin" });
 }
 
 async function postAdmin(req, res) {
-    try {
-
-        const { admincode } = req.body
-        const userId = req.user.id;
-        if (admincode === "admin123") {
-            await updateAdminStatus(userId);
-            res.redirect('/');
-        } else {
-            throw new Error("Invalid admin code");
-        }
-    } catch (err) {
-        throw new Error(err);
+  try {
+    const { admincode } = req.body;
+    const userId = req.user.id;
+    if (admincode === "admin123") {
+      await updateAdminStatus(userId);
+      res.redirect("/");
+    } else {
+      throw new Error("Invalid admin code");
     }
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
-
-
 async function deleteMessage(req, res) {
-    const { id: messageId } = req.params;
-    const isDeleted = await deleteMessageById(messageId);
-    if (isDeleted) {
-        res.redirect('/');
-    } else {
-        res.send("<p class='error'>message not deleted return <a href='/'>home</a></p>")
-    }
+  const { id: messageId } = req.params;
+  const isDeleted = await deleteMessageById(messageId);
+  if (isDeleted) {
+    res.redirect("/");
+  } else {
+    res.send(
+      "<p class='error'>message not deleted return <a href='/'>home</a></p>"
+    );
+  }
 }
 
 const postNewMsg = [
-    messageValidation,
-    async function (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).render('newmsg', { pageTitle: "New Message", errors: errors.array(), });
-        }
-        const { title, message } = req.body;
-        try {
-            await insertNewMsg(req.user.id, title, message);
-            res.redirect('/');
-        } catch (error) {
-            throw new Error(error);
-        }
+  messageValidation,
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("newmsg", { pageTitle: "New Message", errors: errors.array() });
     }
-]
+    const { title, message } = req.body;
+    try {
+      await insertNewMsg(req.user.id, title, message);
+      res.redirect("/");
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+];
 
-module.exports = { getHomePage, deleteMessage, getSignUp, postSignUp, getLogIn, postLogIn, logOutUser, getMembership, postMembership, postAdmin, getAdmin, getNewMsg, postNewMsg }
+module.exports = {
+  getHomePage,
+  deleteMessage,
+  getSignUp,
+  postSignUp,
+  getLogIn,
+  postLogIn,
+  logOutUser,
+  getMembership,
+  postMembership,
+  postAdmin,
+  getAdmin,
+  getNewMsg,
+  postNewMsg,
+};
